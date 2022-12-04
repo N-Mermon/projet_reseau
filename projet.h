77bf0da840a7b4e6d9eb55c3eeca18712623e96f
LDorinfo <119746225+LDorinfo@users.noreply.github.com>
2022-12-04 17:08:33 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -7,21 +7,29 @@ typedef struct ethernet{
 	char type[4];
 } Ethernet;
 
+typedef struct option{
+	char op[3];
+	int octet; // pour L'option 0x07
+	struct option * suiv; 
+}Option; 
+
 typedef struct ipv4{
 	char iHL[2]; 
 	char totalLength[5]; 
 	char identifier[5]; 
-	char flags[3]; 
+	char flags[5]; 
 	char fragmentOffset[4]; //4 avec un pour le caratère de fin de chaine
 	char ttl[3]; 
 	char Protocol[3]; 
 	char headerChecksum[5];
 	char destAddress[9]; 
 	char sourceAddress[9];
-	char * option;  
+	Option* option;  
 } IPV4; 
 
 char* motsansespace(char*x ); 
+int hexa_int(char* s);
+void afficheoption(Option * option); 
 
 // Ethernet 
 Ethernet* lectureEthernet(char* chaine); 
@@ -30,26 +38,8 @@ Ethernet* lecture(char *name);
 void freeEthernet(Ethernet* ether); 
 
 //IPV4
-void lectureIPV4(char* chaine, IPV4* ipv4); 
+int lectureIPV4(char* chaine, IPV4* ipv4); 
 void afficheIPV4(IPV4* ipv4); 
 void freeIPV4(IPV4* ipv4 ); 
-/*typedef struct Reseau{
-	Element_alignement_bar *premier_el;
-	Element_alignement_bar *dernier_el;
-	} Alignement_bar;
-
-typedef struct alignement{
-	Alignement_bar* x;
-	Alignement_bar* y;
-	} Alignement ;
-
-typedef struct optimal{
-	Alignement *al;
-	int dist;
-} Optimal
-
-int Dist_Naif ( char* x, char*y, int taillex, int tailley);
-int Dist_Naif_Rec (char* x, char*y, int i, int j, int c, int dist, int taillex, int tailley);
-*/
 
 #endif
