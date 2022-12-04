10be390f620eb2452e6fadf729ee53b0333e88e9
LDorinfo <119746225+LDorinfo@users.noreply.github.com>
2022-12-04 22:15:02 +0100

    Update ethernet.c

--- ethernet.c
+++ ethernet.c
@@ -10,9 +10,10 @@
 Ethernet* lectureEthernet(char* chaine){
     Ethernet* ethernet = malloc(sizeof(Ethernet));
     for(int i=0; chaine[i]!='\0'; i++){
-        if((i<16)&&(i>=4)) ethernet->mac_source[i-4]=chaine[i]; 
-        if((i<28)&&(i>=16)) ethernet->mac_dest[i-16]=chaine[i]; 
-        if((i<32)&&(i>=28)) ethernet->type[i-28]=chaine[i]; 
+        if((i<12)&&(i>=0)) ethernet->mac_source[i]=chaine[i]; 
+        if((i<24)&&(i>=12)) ethernet->mac_dest[i-12]=chaine[i]; 
+        if((i<28)&&(i>=24)) ethernet->type[i-24]=chaine[i]; 
+        if(i>28) break; 
     }
     ethernet->mac_dest[12]='\0'; 
     ethernet->mac_source[12]='\0'; 
