ea0ac8253eac08a4f49da426e652fdc5024e804f
LDorinfo <119746225+LDorinfo@users.noreply.github.com>
2022-12-07 22:55:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,18 @@
 #include "projet.h"
 
 int main(){
-    Trame* trame= lecture("trame1.txt"); 
-    Trame* trame1= lecture("trame2.txt"); 
-    trame->suiv=trame1; 
-    freeTrame(trame);  
+    //Trame* trame= lecture("trame1.txt"); 
+    //Trame* trame1= lecture("trame2.txt"); 
+    //httptoAscii(trame->http); 
+    //afficheTrame(trame); 
+    //printf(" je suis : %c", 73);
+    //trame->suiv=trame1; 
+    //printf("affiche HTTP corps : %s", trame->http->corpsreq); */
+
+    Trame**t = lirefichier("trame1.txt"); 
+    printf("Je passe "); 
+    afficheTrame(t[0]); 
+    //hextoAscii("73207475"); 
+    //freeTrame(trame);  
     return 0; 
 }
