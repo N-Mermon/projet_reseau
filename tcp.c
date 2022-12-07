a9a711b263f35d6007da6e9dd7618cfaf4985801
LDorinfo <119746225+LDorinfo@users.noreply.github.com>
2022-12-07 11:27:41 +0100

    Update tcp.c

--- tcp.c
+++ tcp.c
@@ -14,8 +14,16 @@ void freeTCP(TCP* tcp){
 	free(tcp->window); 
 	free(tcp->checksum); 
 	free(tcp->urgentPointer); 
-	free(tcp->options); 
+	freeOptionsTCP(tcp->options); 
     free(tcp);
+}
+void freeOptionsTCP(OptionTCP * op){
+    while(op){
+        free(op->longueur); 
+        free(op->type); 
+        free(op->valeur); 
+        op=op->suiv; 
+    }
 }
 void afficheOptionTCP(OptionTCP* op){
     if(op==NULL) return; 
@@ -23,6 +31,16 @@ void afficheOptionTCP(OptionTCP* op){
         printf("Type: %s, longueur: %s, valeur : %s\t", op->type, op->longueur, op->valeur); 
         op=op->suiv; 
     }
+}
+void ajoutOpfin(OptionTCP* ops, OptionTCP* op){
+    if(ops==NULL) {
+        ops=op; 
+        ops->tete=ops; 
+    }
+    if(ops){
+        ops=ops->suiv; 
+    }
+    op->tete=ops->tete; 
 }
 void afficheTCP(TCP* tcp){
     printf("TCP NportS : %s, NportD : %s, SN: %s, AN: %s, ", tcp->sourcePort, tcp->destPort, tcp->sn, tcp->an); 
@@ -41,8 +59,8 @@ int lecturetcp(char* chaine, TCP* tcp){
     tcp->window[4]='\0'; 
     tcp->checksum[4]='\0'; 
     tcp->urgentPointer[4]='\0'; 
+    tcp->options=NULL;     
 
-    OptionTCP* tete=tcp->options; 
     for(int i=0; i<m; i++ ){
         if(i<4 && i>=0) tcp->sourcePort[i]=chaine[i]; 
         if(i<8 && i>=4) tcp->destPort[i-4]=chaine[i]; 
@@ -58,96 +76,96 @@ int lecturetcp(char* chaine, TCP* tcp){
         if(i<40 && i>=36) tcp->urgentPointer[i-36]= chaine[i]; 
         //Les options TCP
         if(i>=40 && (tcp->tHL-20>0)){
-            printf("il y a des options"); 
+            //printf("il y a des options"); 
             if(chaine[i]=='0' && chaine[i+1]=='0'){
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[1]= chaine[i+1]; 
-                tete->type[2]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[1]= chaine[i+1]; 
+                op->type[2]= '\0'; 
                 i++; 
                 return i; 
             }
             if(chaine[i]=='0' && chaine[i+1]=='2'){
                 //MSS
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[0]= chaine[i+1]; 
-                tete->type[2]= '\0'; 
-                tete->longueur[0]= chaine[i+2]; 
-                tete->longueur[1]= chaine[i+3]; 
-                tete->longueur[2]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[0]= chaine[i+1]; 
+                op->type[2]= '\0'; 
+                op->longueur[0]= chaine[i+2]; 
+                op->longueur[1]= chaine[i+3]; 
+                op->longueur[2]= '\0'; 
                 i=i+4; 
-                int j=hexa_int(tete->longueur); 
+                int j=hexa_int(op->longueur); 
                 int k=0; 
                 while(j!=0){
-                    tete->valeur[k]=chaine[i]; 
+                    op->valeur[k]=chaine[i]; 
                     j--; 
                     k++; 
                     i++; 
                 }
-                tete->type[k]= '\0'; 
-                tete=tete->suiv; 
+                op->type[k]= '\0'; 
+                ajoutOpfin(tcp->options, op); 
             }
             if(chaine[i]=='0' && chaine[i+1]=='4'){
                 //SACK
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[0]= chaine[i+1]; 
-                tete->type[2]= '\0'; 
-                tete->longueur[0]= chaine[i+2]; 
-                tete->longueur[1]= chaine[i+3]; 
-                tete->longueur[2]= '\0'; 
-                tete->valeur[0]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[0]= chaine[i+1]; 
+                op->type[2]= '\0'; 
+                op->longueur[0]= chaine[i+2]; 
+                op->longueur[1]= chaine[i+3]; 
+                op->longueur[2]= '\0'; 
+                op->valeur[0]= '\0'; 
                 i=i+3; 
-                tete=tete->suiv; 
+                ajoutOpfin(tcp->options, op);
             }
             if(chaine[i]=='0' && chaine[i+1]=='8'){
                 //Timestamp
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[0]= chaine[i+1];
-                tete->type[2]= '\0';  
-                tete->longueur[0]= chaine[i+2]; 
-                tete->longueur[1]= chaine[i+3]; 
-                tete->longueur[2]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[0]= chaine[i+1];
+                op->type[2]= '\0';  
+                op->longueur[0]= chaine[i+2]; 
+                op->longueur[1]= chaine[i+3]; 
+                op->longueur[2]= '\0'; 
                 i=i+4; 
                 int k=0; 
                 while(k!=16){
-                    tete->valeur[k]=chaine[i]; 
+                    op->valeur[k]=chaine[i]; 
                     k++; 
                     i++; 
                 }
-                tete->valeur[k]= '\0'; 
-                tete=tete->suiv; 
+                op->valeur[k]= '\0'; 
+                ajoutOpfin(tcp->options, op);
             }
             if(chaine[i]=='0' && chaine[i+1]=='1'){
                 //NOP
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[0]= chaine[i+1]; 
-                tete->type[2]= '\0'; 
-                tete->longueur[0]= 0; 
-                tete->longueur[1]= 0; 
-                tete->longueur[2]= '\0'; 
-                tete->valeur[0]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[0]= chaine[i+1]; 
+                op->type[2]= '\0'; 
+                op->longueur[0]= 0; 
+                op->longueur[1]= 0; 
+                op->longueur[2]= '\0'; 
+                op->valeur[0]= '\0'; 
                 i++; 
-                tete=tete->suiv; 
+               ajoutOpfin(tcp->options, op);
             }
             if(chaine[i]=='0' && chaine[i+1]=='2'){
                 //MSS
-                tete= malloc(sizeof(OptionTCP)); 
-                tete->type[0]= chaine[i]; 
-                tete->type[0]= chaine[i+1]; 
-                tete->type[2]= '\0'; 
-                tete->longueur[0]= '0'; 
-                tete->longueur[1]= '3'; 
-                tete->longueur[2]= '\0'; 
+                OptionTCP* op= malloc(sizeof(OptionTCP)); 
+                op->type[0]= chaine[i]; 
+                op->type[0]= chaine[i+1]; 
+                op->type[2]= '\0'; 
+                op->longueur[0]= '0'; 
+                op->longueur[1]= '3'; 
+                op->longueur[2]= '\0'; 
                 i=i+3; 
-                tete->valeur[0]=chaine[i+1]; 
-                tete->valeur[1]=chaine[i+2]; 
-                tete->valeur[2]= '\0'; 
+                op->valeur[0]=chaine[i+1]; 
+                op->valeur[1]=chaine[i+2]; 
+                op->valeur[2]= '\0'; 
                 i=i+2; 
-                tete=tete->suiv; 
+                ajoutOpfin(tcp->options, op); 
             }
 
         }
